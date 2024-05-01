4a70e4de6c199a889853a0e25ca193a808553fb4
MortyE2743 <64481374+MortyE2743@users.noreply.github.com>
2024-05-01 19:11:36 +0200

    Added some new overides

--- config.cpp
+++ config.cpp
@@ -14,7 +14,16 @@ class CfgPatches
 			"DZ_Weapons_Firearms_M4",
 			"DZ_Weapons_Firearms_M16A2",
 			"DZ_Weapons_Firearms_Scout",
-			"DZ_Weapons_Magazines"
+			"DZ_Weapons_Magazines",
+			"DZ_Weapons_Firearms_AK74",
+			"DZ_Weapons_Firearms_SSG82",
+			"DZ_Weapons_Firearms_AKM",
+			"DZ_Weapons_Firearms_CZ527",
+			"DZ_Weapons_Firearms_IZH18",
+			"DZ_Weapons_Firearms_SKS",
+			"DZ_Weapons_Firearms_MosinNagant",
+			"DZ_Weapons_Firearms_SVD",
+			"DZ_Weapons_Firearms_VSS"
 		};
 	};
 };
@@ -40,6 +49,7 @@ class CfgMods
 //!Firearms
 class CfgWeapons
 {
+	class BoltActionRifle_ExternalMagazine_Base;
 	class Rifle_Base;
 	class AK101_Base: Rifle_Base
 	{
@@ -149,6 +159,89 @@ class CfgWeapons
 			"TTC_VP_Ammo_556x45_M855",
 			"TTC_VP_Ammo_556x45_M855A1",
 			"TTC_VP_Ammo_556x45_SSAAP"
+		};
+	};
+	class AK74_Base: Rifle_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_545x39",
+			"Ammo_545x39Tracer",
+			"TTC_VP_Ammo_545x39_7N40"
+		};
+	};
+	class SSG82_Base: BoltActionRifle_ExternalMagazine_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_545x39",
+			"Ammo_545x39Tracer",
+			"TTC_VP_Ammo_545x39_7N40"
+		};
+	};
+	class AKM_Base: Rifle_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class CZ527_Base: BoltActionRifle_ExternalMagazine_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class Izh18_Base: Rifle_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class SKS_Base: Rifle_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_762x39",
+			"Mag_CLIP762x39_10Rnd",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class Mosin9130_Base: BoltActionRifle_ExternalMagazine_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_762x54",
+			"Ammo_762x54Tracer",
+			"Mag_CLIP762x54_5Rnd",
+			"TTC_VP_Ammo_762x54_RBT"
+		};
+	};
+	class SVD_Base: Rifle_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_762x54",
+			"Ammo_762x54Tracer",
+			"TTC_VP_Ammo_762x54_RBT"
+		};
+	};
+	class VSS_Base: Rifle_Base
+	{
+		chamberableFrom[]=
+		{
+			"Ammo_9x39",
+			"Ammo_9x39AP",
+			"TTC_VP_Ammo_9x39_PAB"
 		};
 	};
 };
@@ -275,6 +368,105 @@ class CfgMagazines
 			"TTC_VP_Ammo_556x45_M855",
 			"TTC_VP_Ammo_556x45_M855A1",
 			"TTC_VP_Ammo_556x45_SSAAP"
+		};
+	};
+	class Mag_AK74_30Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_545x39",
+			"Ammo_545x39Tracer",
+			"TTC_VP_Ammo_545x39_7N40"
+		};
+	};
+	class Mag_AK74_45Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_545x39",
+			"Ammo_545x39Tracer",
+			"TTC_VP_Ammo_545x39_7N40"
+		};
+	};
+	class Mag_SSG82_5rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_545x39",
+			"Ammo_545x39Tracer",
+			"TTC_VP_Ammo_545x39_7N40"
+		};
+	};
+	class Mag_AKM_30Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class Mag_AKM_Drum75Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class Mag_AKM_Palm30Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class Mag_CZ527_5rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_762x39",
+			"Ammo_762x39Tracer",
+			"TTC_VP_Ammo_762x39_7N27"
+		};
+	};
+	class Mag_SVD_10Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_762x54",
+			"Ammo_762x54Tracer",
+			"TTC_VP_Ammo_762x54_RBT"
+		};
+	};
+	class Mag_VSS_10Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_9x39",
+			"Ammo_9x39AP",
+			"TTC_VP_Ammo_9x39_PAB"
+		};
+	};
+	class Mag_VAL_20Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_9x39",
+			"Ammo_9x39AP",
+			"TTC_VP_Ammo_9x39_PAB"
+		};
+	};
+	class Mag_Vikhr_30Rnd: Magazine_Base
+	{
+		ammoItems[]=
+		{
+			"Ammo_9x39",
+			"Ammo_9x39AP",
+			"TTC_VP_Ammo_9x39_PAB"
 		};
 	};
 };
